5aa0801becac2ad61dc1ede00018a37ec757c203
AdjieArrayan <arrayan54181@gmail.com>
2025-01-24 16:09:29 +0700

    add vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'adjie-portofolio',
   plugins: [react()],
   css: {
     modules: {
