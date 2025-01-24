e5b447f8541bbc490fb14c1893a6bb390cb09bf8
AdjieArrayan <arrayan54181@gmail.com>
2025-01-24 15:35:29 +0700

    Add another file

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:arrayan54181@email.com">arrayan54181@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/AdjieArrayan">github.com/AdjieArrayan</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
